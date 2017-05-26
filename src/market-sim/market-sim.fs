24b5b59c93dde758aa142acca4ff8d642634af12
Matthew Traylor <mhtraylor@hotmail.com>
2017-05-25 20:37:18 -0500

    refactored offer to use records, added tmp gnuplot output

--- src/market-sim/market-sim.fs
+++ src/market-sim/market-sim.fs
@@ -11,16 +11,20 @@ type Commodity =
     | Wheat
 
 type Offer =
-    | Ask of Commodity * int * float<gold>
-    | Bid of Commodity * int * float<gold>
+    | Ask of Details
+    | Bid of Details
+and
+    Details =
+    { commodity : Commodity
+      units     : int
+      price     : float<gold>
+      traderId  : int }
 
-let getPrice = function
-    | Ask (_,_,price)
-    | Bid (_,_,price) -> price
+let getPrice = function | Ask x | Bid x -> x.price
 
-let getUnits = function
-    | Ask (_,units,_)
-    | Bid (_,units,_) -> units
+let getUnits = function | Ask x | Bid x -> x.units
+
+let getTraderId = function | Ask x | Bid x -> x.traderId
 
 let toGold = float >> (*) 1.0<gold>
 
@@ -48,10 +52,17 @@ let close = sort >> average >> conjugate
 
 type MarketCommand =
     | PostOffer of Offer
-    | Close
+    | Close of (Offer * Offer) list AsyncReplyChannel
 
 type MarketState =
     | Open of Offer list * Offer list | Closed of (Offer * Offer) list
+
+let mktCloseEvent (ctx: System.Threading.SynchronizationContext) = 
+    let evt = Event<(Offer * Offer) list>()
+    evt.Publish,fun t -> ctx.Post ((fun _ -> t |> evt.Trigger), null)
+
+let onMarketClose,sendMarketCloseEvent = 
+    mktCloseEvent System.Threading.SynchronizationContext.Current
 
 let market = MailboxProcessor.Start(fun inbox ->
     let rec loop state = async {
@@ -60,19 +71,22 @@ let market = MailboxProcessor.Start(fun inbox ->
             match msg with
             | PostOffer o ->
                 match o with
-                | Bid (c,m,p) as b ->
+                | Bid x as b ->
                     match state with
                     | Open (bids,asks) -> Open (b::bids,asks)
                     | c                -> c
-                | Ask (c,m,p) as a ->
+                | Ask x as a ->
                     match state with
                     | Open (bids,asks) -> Open (bids,a::asks)
                     | c                -> c
-            | Close       ->
+            | Close r      ->
                 match state with
                 | Open (bids,asks) -> (bids,asks) |> close |> Closed
                 | c                -> c
-        do printfn "%A" newState
+                |> fun (Closed trades as c) ->      // <- fix this
+                        r.Reply (trades)
+                        sendMarketCloseEvent trades
+                        c
         return! loop newState }
     loop (Open ([],[])))
 
@@ -91,11 +105,19 @@ let main argv =
         List.init cnt (fun _ -> r.Next(0,20))
     let toGold x = float x * 1.0<gold>
 
-    100 |> rnd |> List.zip (rnd 100)
-    |> List.iter (fun (x,y) -> Bid (Wood, x, toGold y) |> PostOffer |> market.Post)
-    100 |> rnd |> List.zip (rnd 100)
-    |> List.iter (fun (x,y) -> Ask (Wood, x, toGold y) |> PostOffer |> market.Post)
+    1000 |> rnd |> List.zip (rnd 1000)
+    |> List.iter (fun (x,y) -> 
+            Bid { commodity=Wood; units=x; price=toGold y; traderId=0 }
+            |> PostOffer |> market.Post)
+    1000 |> rnd |> List.zip (rnd 1000)
+    |> List.iter (fun (x,y) -> 
+            Ask { commodity=Wood; units=x; price=toGold y; traderId=0 } 
+            |> PostOffer |> market.Post)
 
-    market.Post(Close)
-    System.Threading.Thread.Sleep(10000)
+    let trades = market.PostAndReply(Close)
+    do printfn "Trades: %A" trades
+    let str = trades |> List.fold (fun s (a,b) -> 
+            sprintf "%s\n%d\t%f\t%d\t%f" s (getUnits a) (getPrice a) (getUnits b) (getPrice b)) "AskUnits AskPrice BidUnits BidPrice"
+    use writer = new System.IO.StreamWriter(@"/home/mtraylor/trades.gnuplot")
+    writer.WriteLine(str)
     0 // return an integer exit code
